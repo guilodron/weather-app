0719e22d10e5db744121793e68e87ff91a21f349
Guilherme Rodrigues <guilodron7@gmail.com>
2020-08-14 00:41:42 -0300

    Add main container

--- src/pages/WeatherPage/index.tsx
+++ src/pages/WeatherPage/index.tsx
@@ -3,8 +3,14 @@ import Morning from '../../components/Morning';
 import Sunset from '../../components/Sunset';
 import Night from '../../components/Night';
 
+import { Container } from './styles';
+
 const WeatherPage: React.FC = () => {
-  return <Night />;
+  return (
+    <Container>
+      <Night />
+    </Container>
+  );
 };
 
 export default WeatherPage;
